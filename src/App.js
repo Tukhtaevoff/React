b0b237d09b303908b60d06e6544efb504980e23c
Tukhtaev27 <108573349+Tukhtaev27@users.noreply.github.com>
2022-10-21 22:51:46 +0500

    weather app added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import './App.css';
-import Tab from './Tab/Tab';
+import React from 'react';
+
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
 function App() {
+
+async function getWeatherByLocation(city) {
+
+  const resp = await fetch(url(city));
+  const data = await resp.json()
+
+  const search = document.getElementById("search")
+
+  const data1 = data.main.temp;
+  const formattedData = format(data1);
+
+  const result = capitalizeFirstLetter(search.value);
+
+  const list = document.createElement("ul");
+  list.id = "list";
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `
+  <p>${result}: ${formattedData}°C</p>
+  `;
+  list.appendChild(listItem);
+  document.body.appendChild(list);
+}
+
+function capitalizeFirstLetter(str) {
+
+  // converting first letter to uppercase
+  const capitalized = str.replace(/^./, str[0].toUpperCase());
+
+  return capitalized;
+}
+
+function format(k) {
+  return Math.floor(k - 273.15);
+}
+
+
   return (
-    <div className="App">
-      <Tab />
+    <div className="app">
+       <form id="form" onSubmit={(evt) => {
+        evt.preventDefault();
+        let cityValue = evt.target.elements.search.value;
+        if(cityValue) {
+          getWeatherByLocation(cityValue);
+        }
+        cityValue = '';
+       }}>
+        <input type="text" id="search" placeholder="Search by location"/>
+        <button type='submit'>Search</button>
+    </form>
     </div>
   );
 }
